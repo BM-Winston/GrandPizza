55b57de0f85d16a40777560f47ce07b6c086bcc9
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-03-21 23:16:12 +0300

    patch:remove delivery form

--- scripts.js
+++ scripts.js
@@ -8,7 +8,40 @@ class selectPizza{
 
 }
 
+selectPizza.prototype.selectSizePrice = function () {
+  if (this.size == "Large") {
+    return 1305;
+  } else if (this.size == "Medium") {
+    return 975;
+  } else if (this.size == "Small") {
+    return 645;
+  } 
+};
 
+selectPizza.prototype.selectToppingsPrice = function () {
+  if (
+    this.toppings == "Pepperoni" ||
+    this.toppings == "Supreme" ||
+    this.toppings == "Hawaiian" 
+    
+  ) {
+    return 70;
+  } else {
+    console.log("Choose an option");
+  }
+};
+
+selectPizza.prototype.selectCrustPrice = function () {
+  if (this.crust == "Crispy") {
+    return 250;
+  } else if (this.crust == "Stuffed") {
+    return 245;
+  } else if (this.crust == "Gluten-free") {
+    return 240;
+  } else {
+    console.log("Choose an option");
+  }
+};
 $(document).ready(function () {
     $("#place-order").click(function (event) {
         alert("Thanks for your order! We are waiting for you!");
